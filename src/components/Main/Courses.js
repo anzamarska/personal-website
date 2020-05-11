53237ab0ebef4fb7dd8a41318ade0631d4c8d473
anzamarska <anzamarska@gmail.com>
2020-05-11 16:03:06 +0200

    styled skills section, add courses part

--- src/components/Main/Courses.js
+++ src/components/Main/Courses.js
@@ -9,17 +9,26 @@ const Courses = (props) => (
         <p className="gitText">ACADEMIC EDUCATION</p>
         <ul>
           Jagiellonian University in Cracov Faculty of Philosophy, Masters of
-          Science in Clinical Psychology Graduated in 2019{" "}
+          Science in Clinical Psychology - Graduated in 2019{" "}
         </ul>
         <p className="gitText">now</p>
-        <ul>JPMorgan Chase Software Engineering Virtual Experience</ul>
+        <ul>
+          {" "}
+          I am a trainee participant - Morgan Chase Software Engineering Virtual
+          Experience
+        </ul>
 
         <p className="gitText">Conference that I take part in</p>
         <ul>Lambda Days 13-14 FEBRUARY 2020 </ul>
         <p className="gitText">Workshops</p>
-        <ul>An Ounce of Elixir - 12 FEBRUARY 2020</ul>
+        <ul>Workshops on frontend programming Codecool 20 FEBRUARY 2020</ul>
 
+        <ul>
+          Froncik - basics of creating pages in HTML and CSS 13 FEBRUARY 2020
+        </ul>
+        <ul>An Ounce of Elixir 12 FEBRUARY 2020</ul>
         <p className="gitText">courses</p>
+        <ul>React Challenge in 10 days 1 - 16 APRIL 2020</ul>
         <ul>
           freecodecamp: Responsive Web Design Certification 12 JANUARY - 5
           FEBRUARY 2020
--- src/components/Main/Skills.js
+++ src/components/Main/Skills.js
@@ -7,34 +7,56 @@ const Skills = (props) => (
       <p className="sectionTitle">skills</p>
       <div className="content">
         <p className="gitText">CORE SKILLS </p>
-        <div className="">
-          <img className="skillsIcon" src="html5.svg" alt="" />
-          <p>html</p>
-          <img className="skillsIcon" src="css3.svg" alt="" />
-          <p>css</p>
-          <img className="skillsIcon" src="sass.svg" alt="" />
-          <p>sass</p>
-          <img className="skillsIcon" src="js.svg" alt="" />
-          <p>java script</p>
-          <img className="skillsIcon" src="react.svg" alt="" />
-          <p>react</p>
-          <img className="skillsIcon" src="nodejs.svg" alt="" />
-          <p>node.js</p>
-          <img className="skillsIcon" src="npm.svg" alt="" />
-          <p>npm</p>
-          <img className="skillsIcon" src="slack.svg" alt="" />
-          <p>slack</p>
-          <img className="skillsIcon" src="git.svg" alt="" />
-          <p>git</p>
+        <div className="skillsDiv">
+          <div className="iconDiv">
+            <img className="skillsIcon" src="html5.svg" alt="" />
+            <p className="gitText">html</p>
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="css3.svg" alt="" />
+            <p className="gitText">css</p>{" "}
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="sass.svg" alt="" />
+            <p className="gitText">sass</p>{" "}
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="js.svg" alt="" />
+            <p className="gitText">java script</p>
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="react.svg" alt="" />
+            <p className="gitText">react</p>
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="nodejs.svg" alt="" />
+            <p className="gitText">node.js</p>
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="npm.svg" alt="" />
+            <p className="gitText">npm</p>
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="slack.svg" alt="" />
+            <p className="gitText">slack</p>
+          </div>
+          <div className="iconDiv">
+            <img className="skillsIcon" src="git.svg" alt="" />
+            <p className="gitText">git</p>
+          </div>
         </div>
-        <ul>Basic knowledge of Python</ul>
+        <ul>Basic knowledge of Python and R</ul>
         <ul>Knowledge of IBM SPSS and STATISTICA </ul>
         <ul>Critical thinking and complex problem-solving skills</ul>
         <ul>Extensive active listening and communication skills </ul>
-        <p className="gitText">Languages</p>
-        <ul>English C1 </ul>
-        <ul>German B1 </ul>
-        <ul>Polish native</ul>
+        <div className="skillsDiv">
+          <p className="gitText">Languages</p>
+          <ol className="languages">
+            <li>English C1 </li>
+            <li>German B1 </li>
+            <li>Polish native</li>
+          </ol>
+        </div>
       </div>
     </section>
   </>
