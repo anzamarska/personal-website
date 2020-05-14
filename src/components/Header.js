c0a9378e03f0951ce3a9bc4875831f9a9a611b7f
anzamarska <anzamarska@gmail.com>
2020-05-14 15:05:58 +0200

    add animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
 import React from "react";
 import "../styles/layouts/Header.scss";
+import Roll from "react-reveal/Roll";
+import Bounce from "react-reveal/Bounce";
 
 const Header = (props) => (
   <>
     <div className="header">
-      <button className="button" onClick={props.scrollingViewContact}>
-        contact
-      </button>
-      <button className="button" onClick={props.scrollingViewProjects}>
-        projects
-      </button>
-      <button className="button" onClick={props.scrollingViewCourses}>
-        courses/events
-      </button>
-      <button className="button" onClick={props.scrollingViewSkills}>
-        skills
-      </button>
-      <button className="button" onClick={props.scrollingViewAbout}>
-        about
-      </button>
+      <Bounce left>
+        <button className="button" onClick={props.scrollingViewContact}>
+          contact
+        </button>
+      </Bounce>
+
+      <Roll left>
+        <button className="button" onClick={props.scrollingViewProjects}>
+          projects
+        </button>
+      </Roll>
+      <Bounce bottom>
+        <button className="button" onClick={props.scrollingViewCourses}>
+          courses/events
+        </button>
+      </Bounce>
+      <Bounce right>
+        <button className="button" onClick={props.scrollingViewSkills}>
+          skills
+        </button>
+      </Bounce>
+      <Roll bottom>
+        <button className="button" onClick={props.scrollingViewAbout}>
+          about
+        </button>
+      </Roll>
     </div>
   </>
 );
