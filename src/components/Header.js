e3358aadf85b38f6f9df6f40726b25d2659bde56
anzamarska <anzamarska@gmail.com>
2020-05-15 20:50:45 +0200

    add hamburger to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,32 +6,42 @@ import Bounce from "react-reveal/Bounce";
 const Header = (props) => (
   <>
     <div className="header">
-      <Bounce left>
-        <button className="button" onClick={props.scrollingViewContact}>
-          contact
+      <div className="navigation">
+        <button
+          className="hamburger"
+          onClick={() => props.changeHamburgerClass()}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
         </button>
-      </Bounce>
+        <Bounce left>
+          <button className="button" onClick={props.scrollingViewContact}>
+            contact
+          </button>
+        </Bounce>
 
-      <Roll left>
-        <button className="button" onClick={props.scrollingViewProjects}>
-          projects
-        </button>
-      </Roll>
-      <Bounce bottom>
-        <button className="button" onClick={props.scrollingViewCourses}>
-          courses/events
-        </button>
-      </Bounce>
-      <Bounce right>
-        <button className="button" onClick={props.scrollingViewSkills}>
-          skills
-        </button>
-      </Bounce>
-      <Roll bottom>
-        <button className="button" onClick={props.scrollingViewAbout}>
-          about
-        </button>
-      </Roll>
+        <Roll left>
+          <button className="button" onClick={props.scrollingViewProjects}>
+            projects
+          </button>
+        </Roll>
+        <Bounce bottom>
+          <button className="button" onClick={props.scrollingViewCourses}>
+            courses/events
+          </button>
+        </Bounce>
+        <Bounce right>
+          <button className="button" onClick={props.scrollingViewSkills}>
+            skills
+          </button>
+        </Bounce>
+        <Roll bottom>
+          <button className="button" onClick={props.scrollingViewAbout}>
+            about
+          </button>
+        </Roll>
+      </div>
     </div>
   </>
 );
