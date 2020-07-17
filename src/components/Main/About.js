fc1baa5b31b59c915e4c21419edc0f11d9f910c3
anzamarska <anzamarska@gmail.com>
2020-07-17 17:04:58 +0200

    update dates and deploy new version

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -14,7 +14,8 @@ const About = (props) => (
             people for Information Technology positions. The more I learned
             about this industry, the more I wanted to become part of it. Since
             October 2018, I have been learning independently basics of
-            programming.
+            programming. Currently, I work remotely, still looking for new
+            development opportunities.
           </p>
         </div>
         <img className="profileImg" src="profile.jpg" alt="profile" />
