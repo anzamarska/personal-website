2797c69662d8c87a904e86a9929ac328cd120d51
anzamarska <anzamarska@gmail.com>
2020-09-04 14:00:42 +0200

    change projects available in the section

--- src/components/Main/Projects.js
+++ src/components/Main/Projects.js
@@ -11,7 +11,42 @@ const Projects = (props) => (
           fully implemented, despite of that I hope you will still like them!
         </p>
         <div className="projectDiv">
-          <h3 className="contentTitle">Wallpapers Aplication</h3>
+          <h3 className="contentTitle">Memory Game</h3>
+          <p>
+            The first application I wrote with the help of React. Created
+            together with another Frontend Developer so that I could improve my
+            ability to work with Git Hub.
+          </p>
+          <p className="gitText">code</p>
+          <a
+            aria-label="GitHub-portfolio"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/jolawro/react-memory-game"
+          >
+            <img className="gitHubIcon" src="github.svg" alt="github" />
+          </a>
+          <p className="gitText">demo</p>
+          <div className="demoDiv">
+            <a
+              aria-label="GitHub-portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://awesome-memory-game.netlify.app/"
+            >
+              <img
+                className="projectImg"
+                src="demoProjectImg/projectMemoryImg.png"
+                alt=""
+              />
+            </a>
+          </div>
+          <h6>
+            CSS | HTML | JavaScript | React (styled components) | TypeScript{" "}
+          </h6>
+        </div>
+        <div className="projectDiv">
+          <h3 className="contentTitle">Wallpapers App</h3>
           <p>
             It is an application that allows the user to choose a category
             according to the app will be offered the most beautiful wallpapers
@@ -77,41 +112,6 @@ const Projects = (props) => (
             </a>
           </div>
           <h6>CSS (Sass) | HTML | JavaScript | React </h6>
-        </div>
-        <div className="projectDiv">
-          <h3 className="contentTitle">Memory Game</h3>
-          <p>
-            The first application I wrote with the help of React. Created
-            together with another Frontend Developer so that I could improve my
-            ability to work with Git Hub.
-          </p>
-          <p className="gitText">code</p>
-          <a
-            aria-label="GitHub-portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/jolawro/react-memory-game"
-          >
-            <img className="gitHubIcon" src="github.svg" alt="github" />
-          </a>
-          <p className="gitText">demo</p>
-          <div className="demoDiv">
-            <a
-              aria-label="GitHub-portfolio"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://awesome-memory-game.netlify.app/"
-            >
-              <img
-                className="projectImg"
-                src="demoProjectImg/projectMemoryImg.png"
-                alt=""
-              />
-            </a>
-          </div>
-          <h6>
-            CSS | HTML | JavaScript | React (styled components) | TypeScript{" "}
-          </h6>
         </div>
       </div>
     </section>
